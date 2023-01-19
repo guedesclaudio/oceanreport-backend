44a062bc9a57d2faf3623f441ce8ebaa54db5ab5
guedesclaudio <cgsarmentosilva@gmail.com>
2023-01-19 11:30:54 -0300

    feat(report): added atmosphere conditions

--- src/services/report-service.ts
+++ src/services/report-service.ts
@@ -1,42 +1,82 @@
 import axios from "axios";
-import { OceanData } from "@/types";
+import { OceanData, AtmosphereData } from "@/types";
 
 async function getReportToday() {
-  const data = await getData();
-  const lastData = data.slice(-1)[0];
-  return createReport(lastData);
+  const timestamp = Date.now();
+  const time: string = timestamp.toString();
+  const oceanData = await getOceanData(time);
+  const atmosphereData = await getAtmosphereData(time);
+  const lastOceanData = oceanData.slice(-1)[0];
+  const lastAtmosphereData = atmosphereData.slice(-1)[0];
+  return createOceanReport(lastOceanData) + createAtmosphereReport(lastAtmosphereData);
 }
 
-function createReport(data: OceanData) {
-  let report = `Olá, Cláudio!
-                    Seguem as condições oceânicas.
-                    `;
+function createOceanReport(data: OceanData) {
+  let report = `Olá, Cláudio! \n Seguem as condições oceânicas:`;
   const { Avg_W_Tmp1 } = data;
   const { Hsig } = data;
-  const temperatureCondition = checkTemperatureCondition(Number(Avg_W_Tmp1));
   const waveCondition = checkWaveCondition(Number(Hsig));
-  report += waveCondition;
-  report += temperatureCondition;
-  report += ` \n data: ${data.DAY}/${data.MONTH}/${data.YEAR} / hora: ${data.HOUR}:${data.MINUTE}`;
+  const temperatureCondition = checkTemperatureCondition(Number(Avg_W_Tmp1));
+  report += waveCondition + temperatureCondition;
   return report;
 }
 
+function createAtmosphereReport(data: AtmosphereData) {
+  let report = "\n Seguem as condições meteorológias:";
+  const { Avg_Wnd_Sp } = data;
+  const { Avg_Wnd_Dir_N } = data;
+  const windSpeedCondition = checkWindSpeedCondition(Number(Avg_Wnd_Sp));
+  const windDirectionCondition = "";
+  report += windSpeedCondition;
+  report += ` \n data: ${data.DAY}/${data.MONTH}/${data.YEAR} / hora: ${Number(data.HOUR) - 3}:${data.MINUTE}`;
+  return report;
+}
+
+async function getOceanData(time: string): Promise<OceanData[]> {
+  const url = `https://micro-oceanreport-backend.vercel.app/ocean/${time.slice(0, -3)}`;
+  return (await axios.get(url)).data;
+}
+
+async function getAtmosphereData(time: string) {
+  const url = `https://micro-oceanreport-backend.vercel.app/atmosphere/${time.slice(0, -3)}`;
+  return (await axios.get(url)).data;
+}
+
 function checkTemperatureCondition(temperature: number) {
-  if (Number(temperature) >= 21) return `\n A temperatura da água está quente e se encontra em ${temperature} °C, `;
-  if (Number(temperature) > 18 && Number(temperature) < 21) return `\n A temperatura da água está um pouco gelada e se encontra em é ${temperature} °C`;
-  if (Number(temperature) < 18 && Number(temperature) >= 16) return `\n A temperatura da água está bem gelada e se encontra em é ${temperature} °C`;
-  if (Number(temperature) < 16) return `\n A temperatura da água está muito gelada e se encontra em é ${temperature} °C`;
+  if (temperature >= 21) return `\n A temperatura da água está quente e se encontra em ${temperature} °C.`;
+  if (temperature > 18 && temperature < 21) return `\n A temperatura da água está um pouco gelada e se encontra em ${temperature} °C.`;
+  if (temperature < 18 && temperature >= 16) return `\n A temperatura da água está bem gelada e se encontra em ${temperature} °C.`;
+  if (temperature < 16) return `\n A temperatura da água está muito gelada e se encontra em é ${temperature} °C.`;
 }
 
 function checkWaveCondition(wave: number) {
-  if (Number(wave) <= 0.75) return ` O mar está flat, com ondas em torno de ${wave} metros`;
+  if (wave <= 0.50) return ` \n O mar está muito calmo, com ondas em torno de ${wave} metros.`;
+  if (wave > 0.50 && wave <= 0.75) return ` \n O mar está levemente ondulado, com ondas em torno de ${wave} metros.`;
+  if (wave > 0.75 && wave <= 1.0) return ` \n O mar está com pequenas ondulações em torno de ${wave} metros.`;
+  if (wave > 1.0 && wave <= 1.25) return ` \n O mar está com ondulações um pouco altas, em torno de ${wave} metros.`;
+  if (wave > 1.25 && wave <= 1.50) return ` \n O mar está com ondulações altas, em torno de ${wave} metros.`;
+  if (wave > 1.50 && wave <= 2.0) return ` \n O mar está com um grau alto de agitação, com ondas em torno de ${wave} metros.`;
+  if (wave > 2.00 && wave <= 2.5) return ` \n O mar está de ressaca, com ondas em torno de ${wave} metros. Se atente aos avisos dos bombeiros.`;
+  if (wave > 2.5 && wave <= 3.0) return ` \n O mar está de ressaca forte, com ondas muito altas em torno de ${wave} metros. Se atente aos avisos dos bombeiros.`;
+  if (wave > 3.0 && wave <= 3.5) return ` \n Alerta! O mar está de ressaca muito forte, com ondas muito altas em torno de ${wave} metros. Evite ir a praia.`;
+  if (wave > 4.0) return ` \n Alerta vermelho! O mar está de ressaca muito forte, com ondas muito altas em torno de ${wave} metros. Evite ir a praia.`;
 }
 
-async function getData(): Promise<OceanData[]> {
-  const timestamp = Date.now();
-  const time: string = timestamp.toString();
-  const url = `https://micro-oceanreport-backend.vercel.app/waves/${time.slice(0, -3)}`;
-  return (await axios.get(url)).data;
+function checkWindSpeedCondition(wind: number) {
+  wind = wind * 1.8;
+  if (wind <= 5.0) return ` \n Hoje está praticamente sem vento, em torno de ${wind} Km/h.`;
+  if (wind > 5.0 && wind <= 10.0) return ` \n Hoje está com uma brisa muito leve, em torno de ${wind} Km/h.`;
+  if (wind > 10.0 && wind <= 15.0) return ` \n Hoje está com uma brisa leve, em torno de ${wind} Km/h.`;
+  if (wind > 15.0 && wind <= 20.0) return ` \n Hoje está com ventos fracos, em torno de ${wind} Km/h, podendo ter rajadas mais fortes`;
+  if (wind > 20.0 && wind <= 30.0) return ` \n Hoje está com ventos de intensidade fraca a mediana, em torno de ${wind} Km/h, podendo ter rajadas mais fortes`;
+  if (wind > 30.0 && wind <= 40.0) return ` \n Hoje está com ventos de intensidade mediana, em torno de ${wind} Km/h, podendo ter rajadas mais fortes`;
+  if (wind > 40.0 && wind <= 50.0) return ` \n Hoje está com ventos de intensidade mediana a forte, em torno de ${wind} Km/h, podendo ter rajadas mais fortes`;
+  if (wind > 50.0 && wind <= 60.0) return ` \n Hoje está com ventos de intensidade forte, em torno de ${wind} Km/h, podendo ter rajadas mais fortes`;
+  if (wind > 60.0 && wind <= 70.0) return ` \n Hoje está com ventos de intensidade muito fortes, em torno de ${wind} Km/h, podendo ter rajadas ainda mais fortes`;
+  if (wind > 70.0 && wind <= 80.0) return ` \n Alerta! Hoje está com ventos de intensidade muito fortes, em torno de ${wind} Km/h, podendo ter rajadas ainda mais fortes`;
+  if (wind > 80.0 && wind <= 90.0) return ` \n Alerta de vendaval! Hoje está com ventos de intensidade fortíssima, em torno de ${wind} Km/h, podendo ter rajadas ainda mais fortes`;
+  if (wind > 90.0 && wind <= 100.0) return ` \n Alerta de tempestade ou ciclone extratropical! Hoje está com ventos de intensidade fortíssima, em torno de ${wind} Km/h, podendo ter rajadas ainda mais fortes`;
+  if (wind > 100.0) return ` \n Alerta de ciclone tropical! Hoje está com ventos de intensidade fortíssima, em torno de ${wind} Km/h, podendo ter rajadas ainda mais fortes`;
 }
 
 const reportService = {
@@ -44,4 +84,3 @@ const reportService = {
 };
 
 export default reportService;
-
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,29 @@
 export type OceanData = {
-    Hsig: string,
-    Avg_W_Tmp1: string,
-    Avg_W_Tmp2: string,
-    Avg_Wv_Dir: string,
-    Avg_Wv_Dir_N: string,
-    M_Decl: string,
-    TAvg: string,
-    YEAR: number,
-    MONTH: number,
-    DAY: number,
-    HOUR: number,
-    MINUTE: number,
-    SECOND: number
+    Hsig: string | null,
+    Avg_W_Tmp1: string | null,
+    Avg_W_Tmp2: string | null,
+    Avg_Wv_Dir: string | null,
+    Avg_Wv_Dir_N: string | null,
+    M_Decl: string | null,
+    TAvg: string | null,
+    YEAR: number | null,
+    MONTH: number | null,
+    DAY: number | null,
+    HOUR: number | null,
+    MINUTE: number | null,
+    SECOND: number | null
+};
+
+export type AtmosphereData = {
+    YEAR: number | null,
+    MONTH: number | null,
+    DAY: number | null,
+    HOUR: number | null,
+    MINUTE: number | null,
+    SECOND: string | null,
+    Avg_Air_Tmp: string | number | null,
+    Avg_Wnd_Sp: string | null,
+    Avg_Wnd_Dir: string | null,
+    Avg_Wnd_Dir_N: string | null,
+    M_Decl: string | null
 };
