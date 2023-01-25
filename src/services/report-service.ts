b7195fbf9d3a228f7b8ef252a97b002b13c076f9
guedesclaudio <cgsarmentosilva@gmail.com>
2023-01-25 17:15:35 -0300

    refactor(report): refactored report service

--- src/services/report-service.ts
+++ src/services/report-service.ts
@@ -2,45 +2,57 @@ import axios from "axios";
 import { OceanData, AtmosphereData } from "@/types";
 import { checkTemperatureCondition, checkWaveCondition, checkWindSpeedCondition } from "@/helpers/report-helpers";
 import redis from "@/repositories/redis";
+import { ReportObject } from "@/types";
 
 async function getReportToday() {
   const reportExistsOnRedis = redis.exists("report");
-  if (reportExistsOnRedis) return redis.get("report");
+  
+  if (reportExistsOnRedis) {
+    const response = await redis.get("report");
+    return JSON.parse(response);
+  };
   return "report nao atualizado";
 }
 
-async function generateReport() {
+async function generateReport(): Promise<void> {
   const timestamp = Date.now();
   const time: string = timestamp.toString();
   const oceanData = await getOceanData(time);
   const atmosphereData = await getAtmosphereData(time);
   const lastOceanData = oceanData.slice(-1)[0];
   const lastAtmosphereData = atmosphereData.slice(-1)[0];
-  const report = createOceanReport(lastOceanData) + createAtmosphereReport(lastAtmosphereData);
-  redis.set("report", report);
-  //console.log("Gerou um novo report " + lastOceanData.HOUR  + " horas" + " e " + lastOceanData.MINUTE + " minutos");
+  const report = generateReportObject(lastOceanData, lastAtmosphereData);
+  redis.set("report", JSON.stringify(report));
+  const email = generateEmailReport(report);
+  //console.log(email);
 }
 generateReport();
 setInterval(generateReport, 3600000);
 
-function createOceanReport(data: OceanData) {
-  let report = "Olá, Cláudio! \n Seguem as condições oceânicas:";
-  const { Avg_W_Tmp1 } = data;
-  const { Hsig } = data;
+function generateReportObject(oceanData: OceanData, atmData: AtmosphereData): ReportObject {
+  const { Avg_W_Tmp1 } = oceanData;
+  const { Hsig } = oceanData;
+  const { Avg_Wnd_Sp } = atmData;
   const waveCondition = checkWaveCondition(Number(Hsig));
   const temperatureCondition = checkTemperatureCondition(Number(Avg_W_Tmp1));
-  report += waveCondition + temperatureCondition;
-  return report;
+  const windSpeedCondition = checkWindSpeedCondition(Number(Avg_Wnd_Sp));
+  const reportObject = {
+    waveCondition,
+    temperatureCondition,
+    windSpeedCondition,
+    date: `${atmData.DAY}/${atmData.MONTH}/${atmData.YEAR} `,
+    hour: `${Number(atmData.HOUR) - 3}:${atmData.MINUTE}`
+  }
+  return reportObject;
 }
 
-function createAtmosphereReport(data: AtmosphereData) {
-  let report = "\n Seguem as condições meteorológias:";
-  const { Avg_Wnd_Sp } = data;
-  const { Avg_Wnd_Dir_N } = data;
-  const windSpeedCondition = checkWindSpeedCondition(Number(Avg_Wnd_Sp));
-  const windDirectionCondition = "";
-  report += windSpeedCondition;
-  report += ` \n data: ${data.DAY}/${data.MONTH}/${data.YEAR} / hora: ${Number(data.HOUR) - 3}:${data.MINUTE}`;
+function generateEmailReport(reportObject: ReportObject): string {
+  let report = "Olá, Cláudio! \n Seguem as condições oceânicas:";
+  report += reportObject.waveCondition;
+  report += reportObject.temperatureCondition;
+  report += "\n Seguem as condições meteorológias:";
+  report += reportObject.windSpeedCondition;
+  report += ` \n data: ${reportObject.date} / hora: ${reportObject.hour}`;
   return report;
 }
 
--- tests/integration/report.test.ts
+++ tests/integration/report.test.ts
@@ -12,6 +12,12 @@ describe("GET /report", () => {
   it("should respond with status 200 and report", async () => {
     const response = await server.get("/report");
     expect(response.status).toBe(httpStatus.OK);
-    expect(response.body).toEqual({ report: expect.any(String) });
+    expect(response.body).toEqual({ report: {
+      waveCondition: expect.any(String),
+      temperatureCondition: expect.any(String),
+      windSpeedCondition : expect.any(String),
+      date: expect.any(String),
+      hour: expect.any(String)
+    }});
   });
 });
